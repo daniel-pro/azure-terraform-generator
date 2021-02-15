210a5e79391766ebff616a7bb5a8f2ce7c870538
Daniel Pro <daniel.procopio@gmail.com>
2021-02-15 09:38:47 +0100

    Added Azure.yml, genAZtf.py python script to generate the TF code, templates/provider.tf and templates/resource_group.tf

---
+++ templates/resource_group.tf
@@ -0,0 +1,17 @@
+{%- if azure_resource_groups is defined %}
+
+{% for item in azure_resource_groups %}
+
+resource "azurerm_resource_group" "{{ item.name }}" {
+  name     = "{{ item.name }}"
+  location = "{{ item.location }}"
+{% if item.tags is defined %}
+  tags     = {
+{% for itemtags in item.tags %}
+    {{ itemtags.name }} = "{{ itemtags.value }}"
+{% endfor %}
+   }
+{% endif %}
+{% endfor %}
+{% endif %}
+}
